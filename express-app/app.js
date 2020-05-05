4fcc83c05efc16f043557ceebab2c0823b68b6f4
tako_eng <oys.sys@gmail.com>
2020-05-05 01:12:41 +0000

    test

--- express-app/app.js
+++ express-app/app.js
@@ -1,10 +1,11 @@
-var http = require('http');
-var path = require('path');
-var morgan = require('morgan');
-var express = require('express');
-var bodyParser = require('body-parser');
+const http = require('http');
+const path = require('path');
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-var app = express();
+const app = express();
 
 app.use(morgan("combined"));
 
@@ -27,6 +28,44 @@ app.post("/form", function(req, res){
   return res.render("result", {username: req.body.username, message: req.body.message});
 });
 
-var server = http.createServer(app);
+// debug***************************************
+app.get("/toriki", function(req, res){
+  return res.render("toriki");
+});
+
+app.post("/toriki", function(req, res){
+  const allMenuAllay = [
+    "むね貴族焼（たれ）",
+    "だし巻き",
+    "つくね塩"];
+
+  // jsonファイルを読み込み
+  let jsonObj = JSON.parse(fs.readFileSync(`./torikiMenu.json`, 'utf8'));
+  // let testStr = json.results[0].gender;
+  // console.log(testStr);
+
+  const result = {};
+  jsonObj.list.forEach((obj) => {
+    result[obj.id] = obj;
+  });
+
+    // [object Object]表示を避けるためパース
+    let jsonStr = JSON.stringify(jsonObj)
+
+  // すべてのメニューからランダムに1つ選択する
+  let randomChoiceOne = allMenuAllay[Math.floor(Math.random() * allMenuAllay.length)];
+
+  // リターン
+  return res.render("torikiResult",
+    {
+      menuName: randomChoiceOne,
+      description: "ここに説明",
+      // debug用。あとで消す
+      all: jsonStr
+    });
+});
+// debug***************************************
+
+const server = http.createServer(app);
 server.listen(3000);
 
