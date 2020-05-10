c1d3f4d34e57bbfcf9e75a003535c3f003fa3fbc
tako_eng <oys.sys@gmail.com>
2020-05-10 20:47:31 +0900

    cssを整理

--- php_form/form3.php
+++ php_form/form3.php
@@ -4,20 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../base.css">
-    <title>ホーム | 技研ウェブ</title>
+    <title>完了画面お問い合わせ | 技研ウェブ</title>
 </head>
-
-<!-- ul要素はUnorderd Listの略で、順序を問わないリストを作成する際に使用します。 li要素はリストの各項目を表します。 -->
 
 <body>
     <div id="pagebody">
 
-        <!-- ヘッダ -->
+        <!-- ヘッダ(ホーム) -->
         <div id="header">
-            <h1><a href="../index.html">技研ウェブ</a></h1>
+            <h1><a href="index.html">技研ウェブ</a></h1>
         </div>
 
-        <!-- メインメニュー -->
+        <!-- メニュー -->
         <ul id="menu">
             <li><a href="../index.html">ホーム</a></li>
             <li><a href="../aboutme.html">自己紹介</a></li>
@@ -25,83 +23,78 @@
             <li><a href="../contact.html">お問い合わせ</a></li>
         </ul>
 
-        <!-- ***************** メインメニュー ***************** -->
-        <!-- アプリメニュー -->
-<?php
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
+        <!-- コンテンツ -->
+        <div id="contents">
+            <?php
+            ini_set("display_errors", 1);
+            error_reporting(E_ALL);
 
-session_start();
+            session_start();
 
-//echo "<pre>";
-//var_dump($_POST, $_SESSION);
-//echo "</pre>";
-//die();
+            //echo "<pre>";
+            //var_dump($_POST, $_SESSION);
+            //echo "</pre>";
+            //die();
 
-var_dump(__LINE__);
+            var_dump(__LINE__);
 
-if (isset($_POST['token'], $_SESSION['token']) && ($_POST['token'] === $_SESSION['token'])) {
-    unset($_SESSION['token']);
-    var_dump(__LINE__); //**********debug
-    $name = $_SESSION['name'];
-    $email = $_SESSION['email'];
-    $subject = $_SESSION['subject'];
-    $body = $_SESSION['body'];
+            if (isset($_POST['token'], $_SESSION['token']) && ($_POST['token'] === $_SESSION['token'])) {
+                unset($_SESSION['token']);
+                var_dump(__LINE__); //**********debug
+                $name = $_SESSION['name'];
+                $email = $_SESSION['email'];
+                $subject = $_SESSION['subject'];
+                $body = $_SESSION['body'];
 
-try{
-    $dsn = 'mysql:dbname=contact_form;host=localhost;charset=utf8mb4';
-    $user = 'root';
-    $password = '';
-    var_dump(__LINE__); //**********debug
-    $dbh = new PDO($dsn, $user, $password);
-    var_dump(__LINE__); //**********debug
-    var_dump(__LINE__); //**********debug
-    // var_dump($dbh->errorInfo()); //PDOを使っているとき
-    var_dump(__LINE__); //**********debug
-    var_dump($dbh);
-    //$dbh > query('SET NAMES utf8');
-    $dbh > setAttribute(PDO::ATTR_EMULATE_PREPARES,
-                            false);
-    $sql = 'INSERT INTO inquiries (name,email,subject,body) VALUES( ?, ?, ?, ?)';
-    $stmt = $dbh > prepare($sql);
+                try {
+                    $dsn = 'mysql:dbname=contact_form;host=localhost;charset=utf8mb4';
+                    $user = 'root';
+                    $password = '';
+                    var_dump(__LINE__); //**********debug
+                    $dbh = new PDO($dsn, $user, $password);
+                    var_dump(__LINE__); //**********debug
+                    var_dump(__LINE__); //**********debug
+                    // var_dump($dbh->errorInfo()); //PDOを使っているとき
+                    var_dump(__LINE__); //**********debug
+                    var_dump($dbh);
+                    //$dbh > query('SET NAMES utf8');
+                    $dbh > setAttribute(
+                        PDO::ATTR_EMULATE_PREPARES,
+                        false
+                    );
+                    $sql = 'INSERT INTO inquiries (name,email,subject,body) VALUES( ?, ?, ?, ?)';
+                    $stmt = $dbh > prepare($sql);
 
-    $stmt > bindValue(1, $name, PDO::PARAM_STR);
-    $stmt > bindValue(2, $email, PDO::PARAM_STR);
-    $stmt > bindValue(3, $subject, PDO::PARAM_STR);
-    $stmt > bindValue(4, $body, PDO::PARAM_STR);
-    
-    $stmt > execute();
-    
-    $dbh=null;
-    
-    echo "きちんとしたアクセスです";
-}catch(PDOException $Exception){
-    die('接続エラー：' .$Exception->getMessage());
-    var_dump(__LINE__); //**********debug
-}
-} else {
-    echo "不正なアクセス";
-    header('Location:form1.php');exit();
-}   
-?>
+                    $stmt > bindValue(1, $name, PDO::PARAM_STR);
+                    $stmt > bindValue(2, $email, PDO::PARAM_STR);
+                    $stmt > bindValue(3, $subject, PDO::PARAM_STR);
+                    $stmt > bindValue(4, $body, PDO::PARAM_STR);
 
-    <!doctypehtml>
-    <html>
+                    $stmt > execute();
 
-    <head>
-        <meta charset="utf8">
-        <title>完了画面お問い合わせ</title>
-    </head>
+                    $dbh = null;
 
-    <body>
-        <p>お問い合わせありがとうございます。</p>
-        </body>
+                    echo "きちんとしたアクセスです";
+                } catch (PDOException $Exception) {
+                    die('接続エラー：' . $Exception->getMessage());
+                    var_dump(__LINE__); //**********debug
+                }
+            } else {
+                echo "不正なアクセス";
+                header('Location:form1.php');
+                exit();
+            }
+            ?>
+
+            <p>お問い合わせありがとうございます。</p>
+
+        </div>
+    </div>
+</body>
 
 <!-- フッタ -->
 <div id="footer">
     <small>Copyright (c) 技研ウェブ All Rights Reserved.</small>
 </div>
-</div>
-</body>
 
 </html>
