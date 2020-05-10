6a9a70c6f9914af00ea56b9d9f6a89857982dc6e
tako_eng <oys.sys@gmail.com>
2020-05-10 19:35:20 +0900

    ルーティング、DBを設定
    DB接続はできたがデータの抽出はできず。

--- express-app/app.js
+++ express-app/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const Sequelize = require('sequelize');
 //const mysql = require("mysql");
 
 //
@@ -25,16 +26,16 @@ app.set('view engine', 'ejs');
 
 // *****************************************************************************
 app.get("/", function(req, res){
-  return res.render("index", {title: "hello world"});
+  return res.render("index", {title: "expressアプリのルート"});
  });
 
 // >>フォーム*****************************************************************************
-app.get("/form", function(req, res){
-  return res.render("form");
+app.get("/contacts", function(req, res){
+  return res.render("contacts");
 });
 
-app.post("/form", function(req, res){
-  return res.render("formResult", {username: req.body.username, message: req.body.message});
+app.post("/contactsResult", function(req, res){
+  return res.render("contactsResult", {username: req.body.username, message: req.body.message});
 });
 // <<フォーム*****************************************************************************
 
@@ -44,7 +45,7 @@ app.get("/toriki", function(req, res){
 });
 
 // ガチャ結果
-app.post("/toriki", function(req, res){
+app.post("/torikiResult", function(req, res){
   // jsonファイルを読み込み
   let jsonObj = JSON.parse(fs.readFileSync(`./torikiMenu.json`, 'utf8'));
 
@@ -70,6 +71,30 @@ app.post("/toriki", function(req, res){
 // <<トリキガチャ*****************************************************************************
 
 // >> todolist*****************************************************************************
+// ページ表示
+app.get("/todolist", function(req, res){
+return res.render("todolist");
+});
+
+// 結果
+app.post("/todolistResult", function(req, res){
+  const sequelize = new Sequelize('NodeTest','root','',{dialect:'mysql'});
+
+  /*
+  sequelize.query("select * from test_table").spread((results, metadata) => {
+    console.log(results);
+    sequelize.close();
+  });
+  //*/
+
+  let results = "ここに結果を代入";
+  let metadata = "あとで消す";
+  let seq = sequelize.query("select * from test_table").spread(results, metadata);
+  sequelize.close();
+
+  return res.render("todolistResult", {msg: results});
+});
+
 // const connection = mysql.createConnection({
 //  host: 'localhost',
 // user: 'root',
---
+++ express-app/sequelize-test.js
@@ -0,0 +1,52 @@
+/*
+const Sequelize = require('sequelize');
+
+// 接続先情報
+const host     = 'localhost';
+const database = 'my_db_test1';
+const username = 'root';
+const password = '';
+
+// 接続開始
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: 'mysql',
+  operatorsAliases: false,
+  pool: {
+    min: 0,
+    max: 5,
+    acuire: 30000,
+    idle  : 10000
+  }
+});
+
+// 接続確認用
+sequelize.authenticate()
+  .then(()       => { console.log('Success test connection');        })
+  .catch((error) => { console.log('Failure test connection', error); });
+
+// users テーブルに対応するモデルを作成する
+const UserModel = sequelize.define('users', {                                                                // CREATE TABLE 文で指定した内容は大体以下のような感じ
+  id       : { field: 'id'        , type: Sequelize.INTEGER(11) , primaryKey: true, autoIncrement: true },  // INT(11)       NOT NULL  PRIMARY KEY  AUTO_INCREMENT
+  userName : { field: 'user_name' , type: Sequelize.STRING(100) , allowNull: false                      },  // VARCHAR(100)  NOT NULL
+  address  : { field: 'address'   , type: Sequelize.STRING(500)                                         },  // VARCHAR(500)  DEFAULT NULL
+  createdAt: { field: 'created_at', type: Sequelize.DATE                                                },  // DATETIME      NOT NULL  DEFAULT CURRENT_TIMESTAMP
+  updatedAt: { field: 'updated_at', type: Sequelize.DATE                                                }   // DATETIME      NOT NULL  DEFAULT CURRENT_TIMESTAMP  ON UPDATE CURRENT_TIMESTAMP
+});
+
+// 全件取得してみる
+UserModel.findAll()
+  .then((result) => {
+    console.log('データ取得', result.dataValues);
+  });
+//*/
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('NodeTest','root','',{dialect:'mysql'});
+sequelize.query("select * from test_table").spread((results, metadata) => {
+  console.log(results);
+  sequelize.close();
+});
+
+
+
