20a543c0da15550043c9df2e756538dcb25966cb
tako_eng <oys.sys@gmail.com>
2020-05-16 23:24:49 +0900

    test
    css編集前にコミット

--- express-app/logging.js
+++ express-app/logging.js
@@ -1,16 +1,3 @@
-const log4js = require("log4js");
 // logger.level = 'debug';
 
 // log4jsでは１個のログ出力を appender と呼んでいます。 appender ごとに出力先を変えたり出力フォーマットを変えたりすることができます。
-log4js.configure({
-  appenders: {
-    system: { type: "file", filename: "system.log" },
-  },
-  // カテゴリ→ログの種類のこと。
-  categories: {
-    default: { appenders: ["system"], level: "debug" },
-  },
-});
-const logger = log4js.getLogger("system");
-
-logger.debug(logger);
