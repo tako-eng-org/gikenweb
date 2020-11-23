e4f062a983058b61a3cf067495b0fd79aad41803
tako_eng <oys.sys@gmail.com>
2020-11-23 17:17:27 +0900

    アプリを追加した

---
+++ other-app/choice/choice.js
@@ -0,0 +1,24 @@
+"use strict";
+(function () {
+  let submitbtn = document.getElementById("submitbtn");
+  let do1 = document.getElementById("do1");
+  let do2 = document.getElementById("do2");
+  let result = document.getElementById("result");
+  let reset = document.getElementById("reset");
+
+  //ボタン"決めてもらう"をクリックしたときの処理
+  submitbtn.addEventListener("click", function () {
+    const choiceArray = [do1, do2];
+    let n = Math.floor(Math.random() * choiceArray.length);
+    result.textContent = `「${choiceArray[n].value}」にしましょう。`;
+  });
+
+  reset.addEventListener("click", function () {
+    result.textContent = "結果はここに表示します";
+    do1.value = "";
+    do2.value = "";
+    do1.focus();
+  });
+
+  do1.focus();
+})();
