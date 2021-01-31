b65f50f9d7f481bc3d46404978cf882916ae3b0c
tako_eng <oys.sys@gmail.com>
2021-01-31 14:41:48 +0900

    #19 Update vue.config.js 説明用コメントを追加した

--- goApp/TodoApp/frontend/vue.config.js
+++ goApp/TodoApp/frontend/vue.config.js
@@ -2,6 +2,7 @@
 // htmlWebpackPlugin.options.title（ページタイトル）
 // を修正するために必要な設定ファイル（手動で作成した）。
 module.exports = {
+    // yarnにて資材をビルドする際のモード選択により、パスを変更する
     publicPath: process.env.NODE_ENV === 'production'
         ? '/gin'
         : '/',
@@ -11,6 +12,7 @@ module.exports = {
             title: "Todoアプリ",
         }
     },
+    // webpackによる転送設定
     devServer: {
         proxy: {
             "/gin/": {
