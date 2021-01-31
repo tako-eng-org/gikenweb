3b287e8529aa66cf2213d3a4e8a3728722c75c09
tako_eng <oys.sys@gmail.com>
2021-01-31 11:47:16 +0900

    #19 Update vue.config.js publicPathを追加し、productionモードでのルーティングを設定した。

--- goApp/TodoApp/frontend/vue.config.js
+++ goApp/TodoApp/frontend/vue.config.js
@@ -2,6 +2,9 @@
 // htmlWebpackPlugin.options.title（ページタイトル）
 // を修正するために必要な設定ファイル（手動で作成した）。
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/gin'
+        : '/',
     pages: {
         index: {
             entry: "src/main.js",
@@ -12,6 +15,7 @@ module.exports = {
         proxy: {
             "/gin/": {
                 target: "http://localhost:8000",
+                logLevel: 'debug'
             },
         },
     },
