a1852aa3b84988c2e66f622ffcec137b509c6b02
tako_eng <oys.sys@gmail.com>
2021-02-23 20:44:45 +0900

    #38 Update index.vue 不要なステートメントを削除し整理した

--- goApp/BbsApp/frontend/pages/index.vue
+++ goApp/BbsApp/frontend/pages/index.vue
@@ -1,34 +1,6 @@
 <template>
-  <div class="container">
-    <div>
-      <!--      <Logo/>-->
-<!--      <h1 class="title">-->
-<!--        frontend-->
-<!--      </h1>-->
-      <Bbs></Bbs>
-    </div>
-    <section class="container">
-<!--      <h1 class="h1">Nuxt Bootstrap</h1>-->
-      <b-button variant="primary" href="./detail">詳細をみる</b-button>
-      <NuxtLink to="./detail">go to detail</NuxtLink>
-    </section>
-    <!--      <div class="links">-->
-    <!--        <a-->
-    <!--          href="https://nuxtjs.org/"-->
-    <!--          target="_blank"-->
-    <!--          rel="noopener noreferrer"-->
-    <!--          class="button&#45;&#45;green"-->
-    <!--        >-->
-    <!--          Documentation-->
-    <!--        </a>-->
-    <!--        <a-->
-    <!--          href="https://github.com/nuxt/nuxt.js"-->
-    <!--          target="_blank"-->
-    <!--          rel="noopener noreferrer"-->
-    <!--          class="button&#45;&#45;grey"-->
-    <!--        >-->
-    <!--          GitHub-->
-    <!--        </a>-->
+  <div>
+    <Bbs></Bbs>
   </div>
 </template>
 
