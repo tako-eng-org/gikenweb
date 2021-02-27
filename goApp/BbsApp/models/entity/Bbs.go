c63ba37aac754fbcd358f9bec26979abcd5cb618
tako_eng <oys.sys@gmail.com>
2021-02-27 13:27:06 +0900

    #38 Update Bbs.go

--- goApp/BbsApp/models/entity/Bbs.go
+++ goApp/BbsApp/models/entity/Bbs.go
@@ -2,8 +2,10 @@ package entity
 
 // Bbs はテーブルのモデル
 type Bbs struct {
-	Id    int    `gorm:"primary_key;not null"       json:"id"`
-	Name  string `gorm:"type:varchar(200);not null" json:"name"`
-	Memo  string `gorm:"type:varchar(400)"          json:"memo"`
-	State int    `gorm:"not null"                   json:"state"`
+	Id     int    `gorm:"primary_key;not null"		json:"id"`
+	Theme  string `gorm:"type:varchar(100);not null"	json:"theme"`
+	Title  string `gorm:"type:varchar(200);not null"	json:"title"`
+	Name   string `gorm:"type:varchar(100);not null"	json:"name"`
+	Detail string `gorm:"type:varchar(4000);not null"	json:"detail"`
+	Delkey string `gorm:"type:varchar(20);not null"	json:"delkey"`
 }
